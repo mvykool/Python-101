032492748fbc22324513b3710ef11de7eea67623
mvykool <maykolandres4@gmail.com>
2024-02-19 06:33:41 -0500

    input

--- basics.py
+++ basics.py
@@ -1,12 +1,18 @@
-print('line 1')
-print('line 2')
-print(123)
-print(-2)
-print(2.2)
+# print('line 1')
+# print('line 2')
+# print(123)
+# print(-2)
+# print(2.2)
 
-# comment
+# # comment
 
-# variables
-test_value = 10
-test_value += 50
-print(test_value)
+# # variables
+# test_value = 10
+# test_value += 50
+# print(test_value)
+
+# exercise
+
+user_name = input('whats your name? ')
+
+print(user_name)
