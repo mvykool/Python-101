1168ab394ecc61284cdb2c9aa758cb742abbdf92
mvykool <maykolandres4@gmail.com>
2024-02-25 12:21:47 -0500

    control flows

---
+++ flow.py
@@ -0,0 +1,45 @@
+# # if statements
+
+# if 3 > 4:
+#     print('3 is greater than 4')
+# elif 2 > 1:
+#     print('other result')
+# else:
+#     print('incorrect result')    
+
+# # while loop
+
+# counter = 0
+# while counter <= 10:
+#     print(counter)
+#     counter += 1    
+
+# # for loop
+
+# test_list = [1,2,3,4,5]
+
+# for x in test_list:
+#     print(x)
+
+# # truthy and falsy
+    
+# if 1:
+#     print('truthy')
+# else:
+#     print('falsy')    
+
+# exercise
+    
+flow_list = [1,2,3,4,5]
+
+count = 0
+
+for x in flow_list:
+    if x == 2:
+        print('value is 2')
+    else:
+        print('value is not 2')
+while count <= 6:
+    print('last item')
+    count += 1    
+        
