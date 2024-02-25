762070521f17b3cc3bbe74fd09dd10480a7a1a91
mvykool <maykolandres4@gmail.com>
2024-02-24 23:44:17 -0500

    did solution

--- calculator.py
+++ calculator.py
@@ -1,5 +1,13 @@
+import math
+
+
 height = input('add height')
 width = input('add width')
 
-print(height, width)
+height = int(height)
+width = int(width)
+
+hypotenuse = math.sqrt(height ** 2 + width ** 2)
+
+print(hypotenuse)
 
