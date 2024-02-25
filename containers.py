04d23ab27b5c1051ef17ee0b2cc675b73066b933
mvykool <maykolandres4@gmail.com>
2024-02-25 00:55:08 -0500

    added containers

--- containers.py
+++ containers.py
@@ -1,3 +1,13 @@
-tuple = ('bob', 123, 'lisa', ('another list')) # inmutable
+tuple = ('bob', 123, 'lisa', ('another list')) # immutable
 
 list = ['bob', 123, 'lisa', ('another list')] # mutable
+
+set = {1,2,3, 'test'} # every entry is unique
+
+# key paired, kind of like an object
+
+dictionary = {
+    'name': 'bob',
+    123: 'test',
+    'lisa': (1,2,3)
+}
